f34a0e9fe7fedea5b908e6055a52664dc9674f96
Ipsita-dhar <64036822+Ipsita-dhar@users.noreply.github.com>
2021-07-17 23:40:16 +0530

    ReactJS Day 5 Assignment

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css';
+import NavBar from "./Components/Navbar"
+import Hero from "./Components/Hero"
+import Feature from "./Components/Feature"
+import Touch from "./Components/Touch"
+import Footer from "./Components/Footer"
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Feature />
+      <Touch />
+      <Footer />
+    </div>
+    
+  )
+}
+
+export default App;
